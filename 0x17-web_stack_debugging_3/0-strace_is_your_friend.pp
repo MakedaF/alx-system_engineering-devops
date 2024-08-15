70e762b542d0db6c039e14ffc6d781351a46313e
MakedaF <makedareda@gmail.com>
2024-08-15 13:56:26 +0000

    tap is no good for puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-# script finds and replaces "phpp" extentions to "php" in "wp-settings.php"
-
-exec{'fix-wordpress':
-	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path	=> '/usr/local/bin/:/bin/'
+#script finds and replaces "phpp" extensions with "php" in "wp-settings.php".
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/usr/local/bin', '/bin'],
 }
